3eb8586ec23ac39f14dc00d07859d36e47ed57bb
Anuradha Karuppiah <anuradhak@cumulusnetworks.com>
2020-07-31 06:21:57 -0700

    zebra: temporarily disable setting weak override flag
    This is causing problems with VM move i.e. transition from remote
    neigh to local neigh. This transition involves changing the NUD_STATE
    NUD_NOARP to NUD_STALE. As the weak override prevents changing
    the state from connected (REACHABLE, NOARP, PERMANENT) to STALE.
    
    Ticket: CM-30273
    
    Signed-off-by: Anuradha Karuppiah <anuradhak@cumulusnetworks.com>

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -4729,6 +4729,16 @@ static ssize_t netlink_neigh_update_ctx(const struct zebra_dplane_ctx *ctx,
 		/* local neigh */
 		if (update_flags & DPLANE_NEIGH_SET_STATIC)
 			ext_flags |= NTF_E_MH_PEER_SYNC;
+
+#if 0
+		/* the ndm_state set for local entries can be REACHABLE or
+		 * STALE. if the dataplane has already establish reachability
+		 * (in the meantime) FRR must not over-write it with STALE.
+		 * this accidental race/over-write is avoided by using the
+		 * WEAK_OVERRIDE_STATE
+		 */
+		ext_flags |= NTF_E_WEAK_OVERRIDE_STATE;
+#endif
 	}
 	if (IS_ZEBRA_DEBUG_KERNEL)
 		zlog_debug(
