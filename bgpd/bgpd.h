437674b7bcf869efe5a3b597655dc5c908deeb05
Rajasekar Raja <rajasekarr@nvidia.com>
2024-02-15 11:23:51 -0800

    bgpd : backpressure - Handle BGP-Zebra(EPVN) Install evt Creation
    Current changes deals with EVPN routes installation to zebra.
    
    In evpn_route_select_install() we invoke evpn_zebra_install/uninstall
    which sends zclient_send_message().
    
    This is a continuation of code changes (similar to
    ccfe452763d16c432fa81fd20e805bec819b345e) but to handle evpn part
    of the code.
    
    Ticket: #3390099
    
    Signed-off-by: Rajasekar Raja <rajasekarr@nvidia.com>

--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -547,6 +547,7 @@ struct bgp {
 #define BGP_FLAG_SOFT_VERSION_CAPABILITY (1ULL << 35)
 #define BGP_FLAG_ENFORCE_FIRST_AS (1ULL << 36)
 #define BGP_FLAG_DYNAMIC_CAPABILITY (1ULL << 37)
+#define BGP_FLAG_VNI_DOWN		 (1ULL << 38)
 #define BGP_FLAG_INSTANCE_HIDDEN	 (1ULL << 39)
 /* Prohibit BGP from enabling IPv6 RA on interfaces */
 #define BGP_FLAG_IPV6_NO_AUTO_RA (1ULL << 40)
