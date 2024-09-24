a7372b35f9b877a681fb007239fd4c07528e2bb5
Rajasekar Raja <rajasekarr@nvidia.com>
2024-09-23 21:38:05 -0700

    bgpd: Fix core during received-routes brief json cmd
    When a route-map causes a prefix to be denied (with soft-reconfig
    enabled), executing show bgp nbr xyz received-routes brief json results
    in invoking peer_adj_routes_brief() where prefix_path_count can be 0 and a
    premature free of json happens.
    
    This free of json was originally intended for a no-op condition i.e.
    to free up the json objects which are originally allocated and not used.
    
    Fixing this.
    
    Ticket :#4081784
    
    Signed-off-by: Rajasekar Raja <rajasekarr@nvidia.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -14527,6 +14527,7 @@ static int peer_adj_routes_brief(struct vty *vty, struct peer *peer, afi_t afi, 
 		struct bgp_adj_in *ain = NULL;
 		struct bgp_adj_out *adj = NULL;
 		struct peer_af *paf = NULL;
+		bool json_pfx_populated = false;
 
 		if (type == bgp_show_adj_route_received) {
 			for (ain = dest->adj_in; ain; ain = ain->next) {
@@ -14545,6 +14546,7 @@ static int peer_adj_routes_brief(struct vty *vty, struct peer *peer, afi_t afi, 
 				bgp_prefix_json_info_add(vty, json_flags, json_info,
 							 prefix_path_count, multi_path_count);
 				json_object_object_addf(json_prefix, json_info, "%pFX", rn_p);
+				json_pfx_populated = true;
 			}
 			ain = NULL;
 		} else if (type == bgp_show_adj_route_advertised) {
@@ -14569,12 +14571,14 @@ static int peer_adj_routes_brief(struct vty *vty, struct peer *peer, afi_t afi, 
 								 multi_path_count);
 					json_object_object_addf(json_prefix, json_info, "%pFX",
 								rn_p);
+					json_pfx_populated = true;
 				}
 			}
 			adj = NULL;
 		}
 
-		if (prefix_path_count == 0) {
+		/* Free up the below only if json_pfx is not populated */
+		if (prefix_path_count == 0 && !json_pfx_populated) {
 			json_object_free(json_info);
 			json_object_free(json_flags);
 		}
