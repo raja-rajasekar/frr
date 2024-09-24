c277e153a18d0981c71f82779e001e4e18c05842
Rajasekar Raja <rajasekarr@nvidia.com>
2024-09-23 20:37:39 -0700

    bgpd: Avoid unnecessary creation and free of json object
    In peer_adj_routes_brief(), the 'json' variable is only used for early
    exit i.e. during early warning conditions. No point in creating and
    freeing the json object everytime during non early exit cases (Majority
    of the times)
    
    Ticket :#4081784
    
    Signed-off-by: Rajasekar Raja <rajasekarr@nvidia.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -14485,8 +14485,8 @@ static int peer_adj_routes_brief(struct vty *vty, struct peer *peer, afi_t afi, 
 	json_object *json_prefix = NULL; /* JSON per prefix */
 	struct bgp_dest *dest;
 
-	json = json_object_new_object();
 	if (!peer || !peer->afc[afi][safi]) {
+		json = json_object_new_object();
 		json_object_string_add(json, "warning", "No such neighbor or address family");
 		vty_out(vty, "%s\n", json_object_to_json_string(json));
 		json_object_free(json);
@@ -14494,6 +14494,7 @@ static int peer_adj_routes_brief(struct vty *vty, struct peer *peer, afi_t afi, 
 	}
 	if ((type == bgp_show_adj_route_received || type == bgp_show_adj_route_filtered) &&
 	    !CHECK_FLAG(peer->af_flags[afi][safi], PEER_FLAG_SOFT_RECONFIG)) {
+		json = json_object_new_object();
 		json_object_string_add(json, "warning", "Inbound soft reconfiguration not enabled");
 		vty_out(vty, "%s\n", json_object_to_json_string(json));
 		json_object_free(json);
