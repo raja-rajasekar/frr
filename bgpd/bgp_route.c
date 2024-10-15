fc5deebf6a1a81848e080fbdfa5c95590a9df4c7
Chirag Shah <chirag@nvidia.com>
2024-10-15 15:15:14 -0700

    [CUMULUS]bgpd: remove redundant evpn route withdraw
    BGP EVPN code uses independent zapi to sync nexthop and route.
    This mechanism does not require to unnecessary of uninstall of the
    route before syncing update on the existing route.
    
    This was reintroduced during the CL5.5 merge.
    
    NOTE: This commit is not required to be upstreamed.
    
    Ticket: #4124485
    Testing:
    Manual testing of EVPN route imported routes update to zebra.
    
    EVPN_MH smoke:
    Base CL5.11 rc 9
    
    failed 11, passed 165 skipped 3.
    
    EVPN smoke:
    Failed 10, passed 246 skipped 1
    
    Signed-off-by: Chirag Shah <chirag@nvidia.com>

--- bgpd/bgp_route.c
+++ bgpd/bgp_route.c
@@ -3621,14 +3621,6 @@ static void bgp_process_main_one(struct bgp *bgp, struct bgp_dest *dest,
 			|| new_select->sub_type == BGP_ROUTE_AGGREGATE
 			|| new_select->sub_type == BGP_ROUTE_IMPORTED)) {
 
-			/* if this is an evpn imported type-5 prefix,
-			 * we need to withdraw the route first to clear
-			 * the nh neigh and the RMAC entry.
-			 */
-			if (old_select &&
-			    is_route_parent_evpn(old_select))
-				bgp_zebra_withdraw_actual(dest, old_select, bgp);
-
 			/* For EVPN imported route skip putting route add
 			 * to pending queue rather send directly to zebra.
 			 * else case covers the non EVPN impoted routes.
