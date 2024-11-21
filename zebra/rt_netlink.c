0fced41b6b9fba5f8d1a1cb88bc04f99df2e94a5
Mark Stapp <mjs@cisco.com>
2024-11-21 16:17:06 -0500

    zebra: remove thread-unsafe debug from netlink nhg encoder
    Remove use of vrf name, which isn't thread-safe.
    
    Signed-off-by: Mark Stapp <mjs@cisco.com>

--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -3333,9 +3333,8 @@ ssize_t netlink_nexthop_msg_encode(uint16_t cmd,
 nexthop_done:
 
 			if (IS_ZEBRA_DEBUG_KERNEL)
-				zlog_debug("%s: ID (%u): %pNHv(%d) vrf %s(%u) %s ",
+				zlog_debug("%s: ID (%u): %pNHv(%d) vrf %u %s ",
 					   __func__, id, nh, nh->ifindex,
-					   vrf_id_to_name(nh->vrf_id),
 					   nh->vrf_id, label_buf);
 			if (nh)
 				frrtrace(2, frr_zebra, netlink_nexthop_msg_encode, nh, id);
