119c7a1c0faea9118655d5490bb012a735988fd0
Aprathi K <aprathik@nvidia.com>
2025-05-19 22:22:31 -0700

    bgpd: Changes to include new fields in "show bgp router" command
    Included input/output queue limit, update delay timers, graceful shutdown
    route-map delay timer, etc., global bgp config fields in both json
    and vty outputs.
    
    JSON output -
    
    leaf11# show bgp router json
    {
      "bgpStartedAt":"Thu May 29 05:32:48 2025\n",
      "bgpStartedGracefully":"No",
      "bgpGShutEnabled":false,
      "bgpInMaintenanceMode":"No",
      "bgpInstanceCount":1,
      "bgpWaitForFibSet":false,
      "bgpInputQueueLimit":10000,
      "bgpOutputQueueLimit":10000,
      "bgpUpdateDelayTime":0,
      "bgpEstablishWaitTime":0,
      "bgpRMapDelayTimer":5
    }
    
    VTY output -
    
    leaf11# show bgp router
    BGP started at Thu May 29 05:32:48 2025
    BGP GSHUT is disabled.
    Number of BGP instances (including default): 1
    BGP suppress-fib-pending is disabled.
    BGP Input Queue Limit: 10000
    BGP Output Queue Limit: 10000
    BGP Global Update Delay Timers:
      Update Delay Time: 0s
      Establish Wait Time: 0s
    BGP route-map Delay Timer: 5s
    
    Ticket: #4445777
    Signed-off-by: Aprathi K <aprathik@nvidia.com>

--- bgpd/bgp_vty.c
+++ bgpd/bgp_vty.c
@@ -11729,12 +11729,19 @@ DEFPY(show_bgp_router, show_bgp_router_cmd, "show bgp router [json]",
 	}
 
 	if (uj) {
+		json_object_boolean_add(json, "bgpGShutEnabled",
+					CHECK_FLAG(bm->flags, BM_FLAG_GRACEFUL_SHUTDOWN));
+	} else {
+		vty_out(vty, "BGP GSHUT is %s.\n",
+			CHECK_FLAG(bm->flags, BM_FLAG_GRACEFUL_SHUTDOWN) ? "enabled" : "disabled");
+	}
+
+	if (uj) {
 		json_object_string_add(json, "bgpInMaintenanceMode",
 				       (CHECK_FLAG(bm->flags, BM_FLAG_MAINTENANCE_MODE)) ? "Yes"
 											 : "No");
 		json_object_int_add(json, "bgpInstanceCount", listcount(bm->bgp));
 
-		vty_json(vty, json);
 	} else {
 		if (CHECK_FLAG(bm->flags, BM_FLAG_MAINTENANCE_MODE))
 			vty_out(vty, "BGP is in Maintenance mode (BGP GSHUT is in effect)\n");
@@ -11743,6 +11750,30 @@ DEFPY(show_bgp_router, show_bgp_router_cmd, "show bgp router [json]",
 			listcount(bm->bgp));
 	}
 
+	if (uj) {
+		json_object_boolean_add(json, "bgpWaitForFibSet", bm->wait_for_fib);
+	} else {
+		vty_out(vty, "BGP suppress-fib-pending is %s.\n",
+			bm->wait_for_fib ? "enabled" : "disabled");
+	}
+
+	if (uj) {
+		json_object_int_add(json, "bgpInputQueueLimit", bm->inq_limit);
+		json_object_int_add(json, "bgpOutputQueueLimit", bm->outq_limit);
+		json_object_int_add(json, "bgpUpdateDelayTime", bm->v_update_delay);
+		json_object_int_add(json, "bgpEstablishWaitTime", bm->v_establish_wait);
+		json_object_int_add(json, "bgpRMapDelayTimer", bm->rmap_update_timer);
+		vty_json(vty, json);
+	} else {
+		vty_out(vty, "BGP Input Queue Limit: %d\n", bm->inq_limit);
+		vty_out(vty, "BGP Output Queue Limit: %d\n", bm->outq_limit);
+
+		vty_out(vty, "BGP Global Update Delay Timers:\n");
+		vty_out(vty, "  Update Delay Time: %ds\n", bm->v_update_delay);
+		vty_out(vty, "  Establish Wait Time: %ds\n", bm->v_establish_wait);
+
+		vty_out(vty, "BGP route-map Delay Timer: %ds\n", bm->rmap_update_timer);
+	}
 	return CMD_SUCCESS;
 }
 
